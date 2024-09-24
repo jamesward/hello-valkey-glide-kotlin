8136b5c54eba300a43d340cc87d29ddab037c7e4
James Ward <james@jamesward.com>
2024-09-24 15:03:10 -0600

    bump valkey

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "2.0.20"
-    id("com.google.osdetector") version "1.7.3"
+    //id("com.google.osdetector") version "1.7.3"
 }
 
 kotlin {
@@ -15,7 +15,7 @@ application {
 dependencies {
     // might only work for linux-x86_64
     // see: https://github.com/valkey-io/valkey-glide/pull/1905#issuecomment-2219538070
-    implementation("io.valkey", "valkey-glide", "1.0.1", classifier = osdetector.classifier)
+    implementation("io.valkey", "valkey-glide", "1.1.0") //, classifier = osdetector.classifier)
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
